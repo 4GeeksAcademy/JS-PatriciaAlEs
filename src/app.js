f8546c5ee8671d84110f27c2972d0351d3b501cf
Patricia Alvarez <168550797+PatriciaAlEs@users.noreply.github.com>
2025-02-12 10:05:36 +0000

    Practica DOM PatriciaAlEs

--- src/app.js
+++ src/app.js
@@ -22,26 +22,3 @@
 
 // BUCLES (bucle for!!! for... off, for... in)
 
-window.onload = function () {
-    alert("Bienvenidos a mi pagina de likes :D")
-  let contador = 0 
-
-    const divContador = document.getElementById("contador")
-    const boton = document.querySelector(".botonLike")
-    const botondisLike = document.querySelector(".botonDislike")
-
-    function sumarLikes () {
-          contador++
-          divContador.innerHTML = "Like: " + contador
-
-    }
-
-    function restarLikes () {
-      contador--
-      divContador.innerHTML = "Like: " + contador
-    }
-    
-  botondisLike.onclick = restarLikes
-  boton.onclick = sumarLikes
-
-}
