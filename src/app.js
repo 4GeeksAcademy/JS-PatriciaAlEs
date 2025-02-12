92d1ee112f9e5568975de1d51623ee158322348b
Patricia Alvarez <168550797+PatriciaAlEs@users.noreply.github.com>
2025-02-12 10:04:54 +0000

    DOM PatriciaAlEs

--- src/app.js
+++ src/app.js
@@ -1,11 +1,47 @@
-import "bootstrap";
-import "./style.css";
+// TIPOS DE DATOS
+// string, boolean, number, null, undefined, array, objeto
+
+// OPERADORES
+// aritmeticos, comparacion, logicos 
+
+// OPERADOR TERNARIO
+// expresion ? me devuelves esto : me devueves esto otro
+
+// let entradaCine = true
+
+// let puedoEntrar = entradaCine == true ? "puedes pasar" : "no puedes pasar";
+
+// console.log(puedoEntrar);
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// FUNCIONES
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+// CONDICIONALES
+
+
+// BUCLES (bucle for!!! for... off, for... in)
+
+window.onload = function () {
+    alert("Bienvenidos a mi pagina de likes :D")
+  let contador = 0 
+
+    const divContador = document.getElementById("contador")
+    const boton = document.querySelector(".botonLike")
+    const botondisLike = document.querySelector(".botonDislike")
+
+    function sumarLikes () {
+          contador++
+          divContador.innerHTML = "Like: " + contador
+
+    }
+
+    function restarLikes () {
+      contador--
+      divContador.innerHTML = "Like: " + contador
+    }
+    
+  botondisLike.onclick = restarLikes
+  boton.onclick = sumarLikes
+
+}
